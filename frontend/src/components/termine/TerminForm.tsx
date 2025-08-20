a804c7923a356443efb9809eb900349cba47c8d6
vuqu-git <vuqu@fantasymail.de>
2025-08-21 01:24:06 +0200

    Add more about places to save static image files

--- frontend/src/components/termine/TerminForm.tsx
+++ frontend/src/components/termine/TerminForm.tsx
@@ -401,7 +401,11 @@ export default function TerminForm() {
                         <br/>
                         Bilddatei muss unter https://pupille.org/bilder/filmbilder/ abgelegt sein.
                         <br/>
-                        Bilder für Überraschungsfilme: surprise_film1.jpg, ... , surprise_film3.jpg
+                        Bilddatei muss unter /var/www/vhosts/epic-hypatia.212-227-48-226.plesk.page/pupille-website/external-static-container/static-files/bilder/filmbilder abgelegt sein
+                         → plesk file manager nutzen! Achtung: Hier wird dieser Pfad aber als nur als /pupille-website/external-static-container/static-files/bilder/filmbilder dargestellt, weil der plesk file manager /var/www/vhosts/epic-hypatia.212-227-48-226.plesk.page
+                        als Stammverzeichnis hat.
+                        <br/>
+                        Bilder für Überraschungsfilme: surprise_film1.jpg, ... , surprise_film4.jpg
                     </Form.Text>
                 </Form.Group>
 
