f371b35a67f2a0965d4899bc7dfcdc889923b899
vuqu-git <vuqu@fantasymail.de>
2025-09-26 10:28:49 +0200

    Amend render methods to take optional css class as 2nd parameter

--- frontend/src/utils/renderHtmlText.tsx
+++ frontend/src/utils/renderHtmlText.tsx
@@ -1,23 +1,11 @@
-const renderHtmlText = (htmlString: string | null | undefined) => {
+const renderHtmlText = (
+    htmlString: string | null | undefined,
+    className?: string
+) => {
     if (htmlString == null) {
         return undefined; // Or perhaps an empty div: <div />; or some other fallback
     }
-    return <span dangerouslySetInnerHTML={{ __html: htmlString }} />;
+    return <span className={className} dangerouslySetInnerHTML={{ __html: htmlString }} />;
 };
 
 export { renderHtmlText }; // Export the function with the desired name
-
-// // Example usage (ONLY if 'trustedHTML' is guaranteed to be safe):
-// const trustedHTML = "<strong>This is bold and <em>italic</em>.</strong>";
-// const htmlElement = renderDangerousHTML(trustedHTML);
-//
-// // You can then use 'htmlElement' in your JSX:
-// // <div>{htmlElement}</div>
-
-
-
-// const renderDangerousHTML = (htmlString: string) => {
-//     return <div dangerouslySetInnerHTML={{ __html: htmlString }} />;
-// };
-//
-// export { renderDangerousHTML };
