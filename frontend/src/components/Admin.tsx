0b54513ee0a5f154622db6072f41695c6b0048d8
vuqu-git <vuqu@fantasymail.de>
2025-09-26 11:05:28 +0200

    Delete unused import

--- frontend/src/components/Admin.tsx
+++ frontend/src/components/Admin.tsx
@@ -1,7 +1,6 @@
 import {Link} from "react-router-dom";
 import Logout from "./security/Logout.tsx";
 import {Badge} from "react-bootstrap";
-import React from "react";
 
 export default function Admin() {
 
