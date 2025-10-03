632ad4c578714d1408b79ebfa9e64f4293f614db
vuqu-git <vuqu@fantasymail.de>
2025-10-03 22:18:17 +0200

    Fix to correct relativePathForVPSDeployment in utils/config.ts

--- frontend/src/utils/config.ts
+++ frontend/src/utils/config.ts
@@ -1,8 +1,10 @@
-const relativePathForVPSDeployment = ""; // use this to create docker image for render and localhost
-// const relativePathForVPSDeployment = "/static-files/bilder/filmbilder/" // use this to create docker image for VPS docker deployment
+// const relativePathForVPSDeployment = ""; // use this to create docker image FOR render and localhost
+const relativePathForVPSDeployment = "/static-files/" // use this to create docker image FOR VPS docker deployment
 
 // const absolutePathForRenderAndLocalhost = "https://www.pupille.org/bilder/filmbilder/" // to delete after migration to VPS and us this "https://www.pupille.org/static-files/bilder/filmbilder/"
-const absolutePathForRenderAndLocalhost = "https://www.pupille.org/" // to delete after migration to VPS and use this "https://www.pupille.org/static-files/"
+// const absolutePathForRenderAndLocalhost = "https://www.pupille.org/" // to delete after migration to VPS and use this "https://www.pupille.org/static-files/"
+const absolutePathForRenderAndLocalhost = "https://www.pupille.org/static-files/"
+
 export const staticFilePathFrontend = relativePathForVPSDeployment || absolutePathForRenderAndLocalhost;
 
 
