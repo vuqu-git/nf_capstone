e4c7a88c026163ee4e3edb079dec192270a6346b
vuqu-git <vuqu@fantasymail.de>
2025-12-28 21:41:53 +0100

    Unify option/item display in selections incl. date

--- frontend/src/components/programmhefte/ProgrammheftSelection.tsx
+++ frontend/src/components/programmhefte/ProgrammheftSelection.tsx
@@ -31,7 +31,7 @@ export default function ProgrammheftSelection({
     <option value="">{textForDefaultOption}</option>
     {allProgrammhefte.map((p: ProgrammheftDTOSelection) => (
         <option key={p.pnr} value={p.pnr}>
-        {`${p.titel}, bis ${p.gueltigBis}`}
+        {`🔚 ${p.gueltigBis} | ${p.titel}`}
         </option>
     ))}
     </Form.Select>
