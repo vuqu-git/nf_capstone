f158d4aac5e97e37f558aa59c9b47e3538a79df0
vuqu-git <vuqu@fantasymail.de>
2026-01-15 17:09:56 +0100

    Update to v1.1.6

--- frontend/src/utils/config.ts
+++ frontend/src/utils/config.ts
@@ -1,9 +1,10 @@
-// const relativePathForVPSDeployment = ""; // use this to create docker image FOR render and localhost
-const relativePathForVPSDeployment = "/static-files/" // use this to create docker image FOR VPS docker deployment
+export const avgDurationTrailer = 12;
 
-// ----------------------------------
+// ---------------------------------------------------
 
-// const absolutePathForRenderAndLocalhost = "https://www.pupille.org/bilder/filmbilder/" // to delete after migration to VPS and us this "https://www.pupille.org/static-files/bilder/filmbilder/"
+const relativePathForVPSDeployment = ""; // use this to create docker image FOR render and localhost
+// const relativePathForVPSDeployment = "/static-files/" // use this to create docker image FOR VPS docker deployment
+
 const absolutePathForRenderAndLocalhost = "https://www.pupille.org/static-files/"
 
 export const staticFilePathFrontend = relativePathForVPSDeployment || absolutePathForRenderAndLocalhost;
